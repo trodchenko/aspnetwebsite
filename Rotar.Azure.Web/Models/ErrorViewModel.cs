e43410c67464a4e87cd2eca234630f96ccf05436
GODELTECH\t.rodchanka <t.rodchanka@godeltech.com>
2019-05-31 17:20:49 +0300

    Add default asp.net core website

---
+++ Rotar.Azure.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Rotar.Azure.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
